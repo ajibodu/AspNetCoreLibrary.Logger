c2ed090b1cf4b53873ee39d38caf4f6de2f0396d
Emmanuel Ajibodu <emmanuel.ajibodu@zenithbank.com>
2020-11-17 15:16:25 +0100

    first commit

---
+++ Implementation/Log.cs
@@ -0,0 +1,79 @@
+﻿using Logger.Interface;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger.Implementation
+{
+    public class Log : ILog
+    {
+        private static readonly object _object = new object();
+        private static readonly object _objectex = new object();
+        private readonly string path;
+        public Log(string filepath)
+        {
+            path = filepath;
+        }
+        public void logMessage(string source, string request, string response = null)
+        {
+            Task.Run(() => logM(source, request, response));
+        }
+
+        public void logException(string source, Exception ex)
+        {
+            Task.Run(() => logE(source, ex));
+        }
+        private void logM(string source, string request, string response = null)
+        {
+            string filename = path + "\\" + DateTime.Now.ToString("yyyy") + "\\" + DateTime.Now.ToString("MM") + "\\";
+
+            if (!File.Exists(filename))
+            {
+                Directory.CreateDirectory(filename);
+            }
+            lock (_object)
+            {
+                using (TextWriter tw = new StreamWriter(filename  + DateTime.Now.ToString("yyyyMMdd") + "_Log.txt", true))
+                {
+                    tw.WriteLine("Date        : " + DateTime.Now.ToLongDateString() + " " + DateTime.Now.TimeOfDay.ToString());
+                    tw.WriteLine("SOURCE      : " + source);
+                    if (!string.IsNullOrEmpty(request))
+                    {
+                        tw.WriteLine("REQUEST     : " + request);
+                    }
+                    if (!string.IsNullOrEmpty(response))
+                    {
+                        tw.WriteLine("RESPONSE    : " + response);
+                    }
+                    tw.WriteLine();
+                    tw.Close();
+                }
+            }
+        }
+
+        private void logE(string source, Exception exception)
+        {
+            string filename = path + "\\" + DateTime.Now.ToString("yyyy") + "\\" + DateTime.Now.ToString("MM") + "\\";
+
+            if (!File.Exists(filename))
+            {
+                Directory.CreateDirectory(filename);
+            }
+            lock (_objectex)
+            {
+                using (TextWriter tw = new StreamWriter(filename + DateTime.Now.ToString("yyyyMMdd") + "_Exception.txt", true))
+                {
+                    tw.WriteLine("Date      :      " + DateTime.Now.ToLongDateString() + " " + DateTime.Now.TimeOfDay.ToString());
+                    tw.WriteLine("SOURCE    :      " + source);
+                    tw.WriteLine("EXCEPTION :      " + exception.Message);
+                    tw.WriteLine("INNEREXCEPTION : " + exception.InnerException);
+                    tw.WriteLine("STACKTRACE : " + exception.StackTrace);
+                    tw.WriteLine();
+                    tw.Close();
+                }
+            }
+        }
+    }
+}
---
+++ Interface/ILog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Logger.Interface
+{
+    public interface ILog
+    {
+        public void logMessage(string source, string request, string response = null);
+
+        public void logException(string source, Exception ex);
+    }
+}
